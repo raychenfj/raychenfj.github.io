f89ae4d2f77e2259491c101a3bfe76dca4ceaa80
陈逢隽 <raychenfj@gmail.com>
2018-12-04 20:26:09 +0800

    feat: add nomisma

---
+++ nomisma/precache-manifest.3fc4c53c348cbfc36da1be8b14a822a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c572eb984654008b6276",
+    "url": "./static/js/main.c572eb98.chunk.js"
+  },
+  {
+    "revision": "3bab7d444d36fc165665",
+    "url": "./static/js/1.3bab7d44.chunk.js"
+  },
+  {
+    "revision": "c572eb984654008b6276",
+    "url": "./static/css/main.189db5ac.chunk.css"
+  },
+  {
+    "revision": "9a8a669d6480c908e31ee32a022d16f0",
+    "url": "./index.html"
+  }
+];
